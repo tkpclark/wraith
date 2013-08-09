23ccccbd38dbaa84172bf251c7254a8260764f7d
tkpclark <tengkeping@gmail.com>
2013-08-09 16:57:08 +0800

    enable IsmpSpEngine

--- gw/ctcc/ISMP/IsmpSpEngine.c
+++ gw/ctcc/ISMP/IsmpSpEngine.c
@@ -1,16 +1,23 @@
-#define _FILE_OFFSET_BITS 64
+//#define _FILE_OFFSET_BITS 64
 #include "soapH.h"
 #include "IsmpSpEngineSoapBinding.nsmap"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ccl/ccl.h>
+#include "mysqllib.h"
+char mdname[]="IsmpSpEngine";
+char logpath[] = "../logs/ctccgw/";
+char version[]="1.00";
 
-char logbuf[1024];
-char mdname[32];
-static char heapfile[128];
-static char logfile[128];
-int logfd;
+static char ip[32];
+static char port[8];
+static char db[32];
+static char user[32];
+static char pass[32];
+static char gwid[4];
+
+static MYSQL mysql;
 
 static void read_config()
 {
@@ -19,33 +26,84 @@ static void read_config()
 	config.comment_char = '#';
 	config.sep_char = '=';
 	config.str_char = '"';
-	ccl_parse(&config, "../conf/IsmpSpEngine.ccl");
+	ccl_parse(&config, "../conf/ctccgw.ccl");
 	while((iter = ccl_iterate(&config)) != 0)
 	{
-		if(!strcmp(iter->key,"logfile"))
-			strcpy(logfile,iter->value);
-		else if(!strcmp(iter->key,"mdname"))
-			strcpy(mdname,iter->value);
-		else if(!strcmp(iter->key,"heapfile"))
-			strcpy(heapfile,iter->value);
+		if(!strcmp(iter->key,"ip"))
+			strcpy(ip,iter->value);
+		else if(!strcmp(iter->key,"db"))
+			strcpy(db,iter->value);
+		else if(!strcmp(iter->key,"user"))
+			strcpy(user,iter->value);
+		else if(!strcmp(iter->key,"pass"))
+			strcpy(pass,iter->value);
+		else if(!strcmp(iter->key,"gwid"))
+			strcpy(gwid,iter->value);
+
 	}
 	ccl_release(&config);
+
+	//proclog("[%s][%s][%s][%s][%s]",ip,db,user,pass,gwid);
+}
+static workdir(char *path)
+{
+        char workdir[128];
+        strcpy(workdir,path);
+        char *p=NULL;
+        p=strrchr(workdir,'/');
+        *p=0;
+        //printf("workdir:%s\n", workdir);
+
+        chdir(workdir);
+
+
 }
 static void my_init()
 {
 	read_config();
-	
-	logfd=open(logfile,O_WRONLY|O_APPEND);
-	if(logfd==-1)
-	{
-		printf("open logfile error!\n");
-		exit(0);
-	}
-	
-	//proclog(logfd,"starting...");
+	mysql_create_connect(&mysql, ip, user,pass,db);
 }
+
+
+int __ns1__orderRelationUpdateNotify(struct soap *soap, struct ns2__OrderRelationUpdateNotifyReq *ns2__OrderRelationUpdateNotifyReq,struct ns3__Response *ns3__Response)
+{
+	proclog("[orderRelationUpdateNotify] sNo[%s]userID[%s]prodID[%s]packID[%s]OPType[%d]",
+		ns2__OrderRelationUpdateNotifyReq->streamingNo,
+		ns2__OrderRelationUpdateNotifyReq->userID,
+		ns2__OrderRelationUpdateNotifyReq->productID,
+		ns2__OrderRelationUpdateNotifyReq->packageID,
+		ns2__OrderRelationUpdateNotifyReq->OPType);
+
+	ns3__Response->streamingNo=ns2__OrderRelationUpdateNotifyReq->streamingNo;
+	    ns3__Response->resultCode=0;
+	
+	 return SOAP_OK;
+}
+
+
+int __ns1__serviceConsumeNotify(struct soap *soap, struct ns2__ServiceConsumeNotifyReq *ns2__ServiceConsumeNotifyReq, struct ns3__Response *ns3__Response)
+{
+	char gbcontent[512];
+	memset(gbcontent,0,sizeof(gbcontent));
+	to_gb(ns2__ServiceConsumeNotifyReq->featureStr,gbcontent);
+	proclog("[serviceConsumeNotify]sNo[%s]featureStr[%s]linkID[%s]userID[%s]productID[%s]",ns2__ServiceConsumeNotifyReq->streamingNo,gbcontent,ns2__ServiceConsumeNotifyReq->linkID,ns2__ServiceConsumeNotifyReq->userID,ns2__ServiceConsumeNotifyReq->productID);
+
+	ns3__Response->streamingNo=ns2__ServiceConsumeNotifyReq->streamingNo;
+	ns3__Response->resultCode=0;
+	return SOAP_OK;
+}
+
+
+int __ns1__notifyManagementInfo(struct soap *soap, struct ns2__NotifyManagementInfoReq *ns2__NotifyManagementInfoReq,struct ns3__NotifyManagementInfoRsp *ns3__NotifyManagementInfoRsp)
+{
+	proclog("[notifyManagementInfo]sNo[%s]ID[%s]",ns2__NotifyManagementInfoReq->streamingNo,ns2__NotifyManagementInfoReq->ID);
+	ns3__NotifyManagementInfoRsp->streamingNo=ns2__NotifyManagementInfoReq->streamingNo;
+	ns3__NotifyManagementInfoRsp->resultCode=0;
+	return SOAP_OK;
+}
+
 int main(int argc, char **argv)
-{ 
+{
 	SOAP_SOCKET m, s; /* master and slave sockets */
 	my_init();
 	struct soap soap;
@@ -58,80 +116,4 @@ int main(int argc, char **argv)
 	soap_set_mode(&soap, SOAP_C_UTFSTRING);
 	soap_serve(&soap);
 	return 0;
-}
-
-
-int __ns1__orderRelationUpdateNotify(struct soap *soap, struct ns2__OrderRelationUpdateNotifyReq *ns2__OrderRelationUpdateNotifyReq,struct ns3__Response *ns3__Response)
-{
-	
-	ns3__Response->streamingNo=ns2__OrderRelationUpdateNotifyReq->streamingNo;
-    ns3__Response->resultCode=0;
-	
-	sprintf(logbuf,"[orderRelationUpdateNotify] sNo[%s]userID[%s]prodID[%s]packID[%s]OPType[%d]",
-		ns2__OrderRelationUpdateNotifyReq->streamingNo,
-		ns2__OrderRelationUpdateNotifyReq->userID,
-		ns2__OrderRelationUpdateNotifyReq->productID,
-		ns2__OrderRelationUpdateNotifyReq->packageID,
-		ns2__OrderRelationUpdateNotifyReq->OPType);
-	proclog(logfd,logbuf);
-	//writing heapfile
-	int fd;
-	fd=open(heapfile,O_WRONLY|O_APPEND);
-	if(fd==-1)
-	{
-		proclog(logfd,"open heapfile error!\n");
-		exit(0);
-	}
-	
-	/*
-	sNo 2
-	userID 100
-	prodID 120
-	packID 150
-	OPType 200
-	*/
-	char buf[256];
-	strcpy(buf,"2");
-	if(ns2__OrderRelationUpdateNotifyReq->streamingNo)
-		strcpy(buf+2,ns2__OrderRelationUpdateNotifyReq->streamingNo);
-	if(ns2__OrderRelationUpdateNotifyReq->userID)
-		strcpy(buf+100,ns2__OrderRelationUpdateNotifyReq->userID);
-	if(ns2__OrderRelationUpdateNotifyReq->productID)
-		strcpy(buf+120,ns2__OrderRelationUpdateNotifyReq->productID);
-	if(ns2__OrderRelationUpdateNotifyReq->packageID)
-		strcpy(buf+150,ns2__OrderRelationUpdateNotifyReq->packageID);
-	
-	char tmp[4];
-	sprintf(tmp,"%d",ns2__OrderRelationUpdateNotifyReq->OPType);
-	strcpy(buf+200,tmp);
-	
-	write(fd,buf,sizeof(buf));
-	close(fd);
-	
-	
-  return SOAP_OK;
-}
-
-
-int __ns1__serviceConsumeNotify(struct soap *soap, struct ns2__ServiceConsumeNotifyReq *ns2__ServiceConsumeNotifyReq, struct ns3__Response *ns3__Response)
-{
-	char gbcontent[512];
-	memset(gbcontent,0,sizeof(gbcontent));
-	to_gb(ns2__ServiceConsumeNotifyReq->featureStr,gbcontent);
-	sprintf(logbuf,"[serviceConsumeNotify]sNo[%s]featureStr[%s]linkID[%s]userID[%s]productID[%s]",ns2__ServiceConsumeNotifyReq->streamingNo,gbcontent,ns2__ServiceConsumeNotifyReq->linkID,ns2__ServiceConsumeNotifyReq->userID,ns2__ServiceConsumeNotifyReq->productID);
-	proclog(logfd,logbuf);
-
-    ns3__Response->streamingNo=ns2__ServiceConsumeNotifyReq->streamingNo;
-    ns3__Response->resultCode=0;
-    return SOAP_OK;
-}
-
-
-int __ns1__notifyManagementInfo(struct soap *soap, struct ns2__NotifyManagementInfoReq *ns2__NotifyManagementInfoReq,struct ns3__NotifyManagementInfoRsp *ns3__NotifyManagementInfoRsp)
-{
-		sprintf(logbuf,"[notifyManagementInfo]sNo[%s]ID[%s]",ns2__NotifyManagementInfoReq->streamingNo,ns2__NotifyManagementInfoReq->ID);
-		proclog(logfd,logbuf);
-    ns3__NotifyManagementInfoRsp->streamingNo=ns2__NotifyManagementInfoReq->streamingNo;
-    ns3__NotifyManagementInfoRsp->resultCode=0;
-    return SOAP_OK;
 }
